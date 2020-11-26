6b642f0ad79717df9b51904c5f58dbcd19ba1c25
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2020-11-26 18:33:05 +0100

    Prezentacja

--- src/wg_net_bogus/wg_net_bogus/Program.cs
+++ src/wg_net_bogus/wg_net_bogus/Program.cs
@@ -1,6 +1,10 @@
-﻿using ConsoleTables;
+﻿using Bogus;
+using Bogus.Extensions;
+using ConsoleTables;
 using System;
 using System.Collections.Generic;
+using Bogus.Extensions.Poland;
+using System.Threading;
 
 namespace wg_net_bogus
 {
@@ -8,24 +12,22 @@ namespace wg_net_bogus
     {
         static void Main(string[] args)
         {
-            ConsoleHelper.DisplayWelcome();
-
-            // GenerateCustomersTest();
-
-            // GenerareMeasuresTest();
+           // GenerateCustomersTest();
+             GenerareMeasuresTest();
 
             Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
         }
 
-      
+   
+
+        // dotnet add package Bogus
 
         private static void GenerateCustomersTest()
         {
-            var customers = new List<Customer>
-            {
+            CustomerFaker customerFaker = new CustomerFaker();
 
-            };
+            var customers = customerFaker.Generate(100);
 
 
             // dotnet add package ConsoleTables
@@ -38,6 +40,28 @@ namespace wg_net_bogus
 
         private static void GenerareMeasuresTest()
         {
+            var sensors = new Faker<Sensor>()
+                .RuleFor(p => p.MacAddress, f => f.Internet.Mac())
+                .RuleFor(p => p.Name, f => f.Hacker.Noun())
+                .Generate(10);
+
+            var meaures = new Faker<Measure>()
+                .RuleFor(p => p.Id, f => f.Random.Guid())
+                .RuleFor(p => p.Value, f => f.Random.Float(0f, 100f))
+                .RuleFor(p => p.Sensor, f => f.PickRandom(sensors))
+                .GenerateForever();
+               
+
+            foreach(var measure in meaures)
+            {
+                measure.Dump();
+
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+            }
+
+
+
+
         }
     }
 
@@ -58,6 +82,26 @@ namespace wg_net_bogus
         public decimal? CreditAmount { get; set; }
         public string Pesel { get; set; }
         public bool IsRemoved { get; set; }
+    }
+
+    public class CustomerFaker : Faker<Customer>
+    {
+        public CustomerFaker()
+        {
+            RuleFor(p => p.Id, f => f.IndexFaker);
+            RuleFor(p => p.FirstName, f => f.Person.FirstName);
+            RuleFor(p => p.LastName, f => f.Person.LastName);
+            RuleFor(p => p.Gender, f => (Gender)f.Person.Gender);
+            RuleFor(p => p.Birthday, f => f.Person.DateOfBirth);
+            RuleFor(p => p.Email, (f,c) => $"{c.FirstName}.{c.LastName}@domain.com");
+            RuleFor(p => p.CustomerType, f => f.PickRandom<CustomerType>());
+            RuleFor(p => p.Description, f => f.Lorem.Sentence(1));
+            RuleFor(p => p.IsRemoved, f => f.Random.Bool(0.2f));
+            RuleFor(p => p.CreditAmount, f => f.Random.Decimal(1m, 1000m).OrNull(f, 0.2f));
+            RuleFor(p => p.Pesel, f => f.Person.Pesel());
+
+
+        }
     }
 
     public enum Gender
